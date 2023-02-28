bf9fca3237e90319881d1c9ef99f5707d61b3f3b
Van Trieu <vanvtrieu@gmail.com>
2023-02-28 15:18:42 -0700

    Added QR code scoring functionality

--- QRHunter/app/src/main/java/com/example/qrhunter/data/repository/QRCodeRepository.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/data/repository/QRCodeRepository.java
@@ -1,5 +1,47 @@
 package com.example.qrhunter.data.repository;
 
-public class QRCodeRepository extends DataRepository {
+import androidx.annotation.NonNull;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class QRCodeRepository {
+    public String hashQR (String qrCode) {
+        return DigestUtils.sha256Hex(qrCode);
+    }
+    public double calculateScore(@NonNull String hash) {
+        double score = 0;
+        char[] array = hash.toCharArray();
+        for (int i = 0; i < array.length-1; i++) {
+            int value = 0;
+            if (array[i] == array [i+1] ) {
+                int j = i +1;
+                while (array[i] == array[j]) {
+                    value += 1;
+                    j += 1;
+                    if (j == array.length) {
+                        break;
+                    }
+                }
+            }
+            if (value > 0) {
+                score += calculatePoints(array[i], value);
+                i += value;
+            }
+        }
+        return score;
+    }
+
+    public double calculatePoints(Character key, Integer value) {
+        double points;
+        int ascii = (int) key;
+        if (ascii < 58) {
+            ascii -= 48;
+        }
+        else {
+            ascii -= 87;
+        }
+        points = Math.pow(ascii, value);
+        return points;
+    }
 
 }
--- QRHunter/app/src/test/java/com/example/qrhunter/ExampleUnitTest.java
+++ QRHunter/app/src/test/java/com/example/qrhunter/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package com.example.qrhunter;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.qrhunter.data.repository.QRCodeRepository;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +13,28 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void calculatePoints_isCorrect() {
+        QRCodeRepository test = new QRCodeRepository();
+        assertEquals(11, test.calculatePoints('b', 1));
+        assertEquals(5, test.calculatePoints('5', 1));
+        assertEquals(81, test.calculatePoints('9', 2));
+        assertEquals(196, test.calculatePoints('e', 2));
+        assertEquals(1331, test.calculatePoints('b', 3));
+        assertEquals(3125, test.calculatePoints('5', 5));
+    }
+    @Test
+    public void hashQR_isCorrect() {
+        QRCodeRepository test = new QRCodeRepository();
+        // Hash given in example is wrong since echo call does not include -n to remove newline character
+        assertEquals("8227ad036b504e39fe29393ce170908be2b1ea636554488fa86de5d9d6cd2c32", test.hashQR("BFG5DGW54"));
+    }
+    @Test
+    public void calculateScore_isCorrect() {
+        QRCodeRepository test = new QRCodeRepository();
+        String qrCode = test.hashQR("BFG5DGW54");
+        // Repeats in hash are 22, 55, 44, 88 which is 2+5+4+8=19
+        assertEquals(19, test.calculateScore(qrCode));
+        // Hash given in project description
+        assertEquals(111, test.calculateScore("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6"));
     }
 }
