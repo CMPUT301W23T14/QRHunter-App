5579db6eb9a66a6a94394501135be2c54b19065e
Max Tan <105672769+MaxTan1228@users.noreply.github.com>
2023-03-09 10:43:20 -0700

    Profile page chekcpoint

--- QRHunter/app/src/main/java/com/example/qrhunter/ui/profile/ProfileFragment.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/ui/profile/ProfileFragment.java
@@ -1,9 +1,6 @@
 package com.example.qrhunter.ui.profile;
 
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.qrhunter.databinding.FragmentProfileBinding;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProfileFragment extends Fragment {
 
@@ -35,21 +25,6 @@ public class ProfileFragment extends Fragment {
 
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-        binding.addButton.setOnClickListener(view -> {
-            profileViewModel.addCount();
-
-            FirebaseFirestore db;
-            // Get a reference to the database
-            db = FirebaseFirestore.getInstance();
-            // Test case for adding a person's username, scores, and QRCodes
-            profileViewModel.addScore(5000);
-            profileViewModel.addScore(1000);
-            profileViewModel.addScore(200);
-            profileViewModel.addQR("test_hash");
-            profileViewModel.addDocument();
-        });
-
-        profileViewModel.getCount().observe(getViewLifecycleOwner(), newInteger -> binding.counter.setText("Number of Documents:" + String.valueOf(newInteger)));
 
         return binding.getRoot();
     }
