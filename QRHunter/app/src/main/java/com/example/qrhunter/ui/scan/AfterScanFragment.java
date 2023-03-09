38fbd68d9c3ceddb5e9831877f2ac96daf16d415
Harry Zhao <wpzhao@ualberta.ca>
2023-03-09 12:30:57 -0700

    fixed adding geolocation button and added the ability to delete picture as well as create a qrCode object after clicking on the checkmark

--- QRHunter/app/src/main/java/com/example/qrhunter/ui/scan/AfterScanFragment.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/ui/scan/AfterScanFragment.java
@@ -5,8 +5,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,11 +28,19 @@ import androidx.navigation.Navigation;
 
 import com.example.qrhunter.MainActivity;
 import com.example.qrhunter.R;
+import com.example.qrhunter.data.model.Comment;
+import com.example.qrhunter.data.model.Location;
+import com.example.qrhunter.data.model.QRCode;
+import com.example.qrhunter.data.repository.QRCodeRepository;
 import com.example.qrhunter.utils.QRCodeName;
 import com.example.qrhunter.utils.QRCodeUtil;
 import com.example.qrhunter.utils.QRCodeVisual;
 import com.example.qrhunter.databinding.FragmentAfterScanBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -37,9 +48,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
  */
 public class AfterScanFragment extends Fragment {
     private FragmentAfterScanBinding binding;
+    private String hashValue;
     private final int MY_PERMISSIONS_REQUEST_CAMERA = 1001;
-
     ActivityResultLauncher<Intent> cameraActivityResultLauncher;
+
+    Bitmap savedPhoto = null;
+
+    // location is 0,0 until user decides to record location
+    private int latitude = 0;
+    private int longitude = 0;
+    boolean deletePhoto = false;
+    boolean canSaveValue = false;
     //
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,7 +71,7 @@ public class AfterScanFragment extends Fragment {
 
         // get the hashValue and set the visual, name and score to this
         // change this part later after the hashValue is implemented
-        String hashValue = scanViewModel.getQRCodeHash().toString();
+        hashValue = scanViewModel.getQRCodeHash().toString();
 
         // display the views
         binding.QRVisual.setText(QRCodeUtil.generateVisualRepresentation(hashValue));
@@ -69,9 +88,14 @@ public class AfterScanFragment extends Fragment {
         // adding the geo location
         binding.addGeoLocationButton.setOnClickListener(view -> {
             // put add geo location on here
+            //...
+
+            // after location has been added
+            Toast.makeText(view.getContext(), "added your location!", Toast.LENGTH_SHORT).show();
+            binding.addGeoLocationButton.setImageResource(R.drawable.checkbox_location);
         });
 
-        // set to get teh image after taking the photo
+        // set to get the image after taking the photo
         cameraActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
@@ -81,10 +105,17 @@ public class AfterScanFragment extends Fragment {
                             // There are no request codes
                             Intent data = result.getData();
                             Bundle bundle = data.getExtras();
+                            savedPhoto = (Bitmap) bundle.get("data");
+                            savedPhoto = Bitmap.createScaledBitmap(savedPhoto, 640, 480, true);
                             Bitmap finalPhoto = (Bitmap) bundle.get("data");
+                            finalPhoto = Bitmap.createScaledBitmap(savedPhoto, 640, 480, true);
                             binding.locationImage.setVisibility(View.VISIBLE);
-                            binding.addPhotoLocationButton.setImageResource(R.drawable.checkbox_location);
+                            binding.addPhotoLocationButton.setImageResource(R.drawable.remove_icon);
                             binding.locationImage.setImageBitmap(finalPhoto);
+
+                            // the next time we click the icon is to delete the photo
+                            deletePhoto = true;
+                            checkRequirements();
                         }
                     }
                 });
@@ -100,13 +131,62 @@ public class AfterScanFragment extends Fragment {
                         MY_PERMISSIONS_REQUEST_CAMERA);
             }
 
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            cameraActivityResultLauncher.launch(intent);
+            // we delete the picture
+            if(deletePhoto == true) {
+                savedPhoto = null;
+                deletePhoto = false;
+                binding.addPhotoLocationButton.setImageResource(R.drawable.add_icon);
+                binding.locationImage.setVisibility(View.INVISIBLE);
+                checkRequirements();
+            }
+            else {
+                // we are not deleting the photo but taking a picture
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraActivityResultLauncher.launch(intent);
+            }
         });
 
+        binding.MoreOptions.setOnClickListener(view -> {
+            if(canSaveValue == true){
+                ArrayList<String> photos = new ArrayList<String>();
+                photos.add(BitMapToString(savedPhoto));
+                Location location = new Location(latitude, longitude, photos);
+                QRCode newQRCode = new QRCode(hashValue, location, null);
+                
+                // add qrcode to database
+                // QRCodeRepository.addQRCode(newQRCode);
+
+                canSaveValue = false;
+                
+                // navigate to somewhere after this is done
+            }
+        });
 
         return binding.getRoot();
     }
 
+    /**
+     * to check the requirements are all met before storing the object to data base and finish
+     * and go back to main
+     */
+    public void checkRequirements(){
+        // no need to add geolocation due to privacy reasons
+        if(savedPhoto != null){
+            canSaveValue = true;
+            binding.MoreOptions.setImageResource(R.drawable.check_mark);
+        }
+        else{
+            canSaveValue = false;
+            binding.MoreOptions.setImageResource(R.drawable.more_options);
+        }
+    }
+
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos =new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
 
 }
