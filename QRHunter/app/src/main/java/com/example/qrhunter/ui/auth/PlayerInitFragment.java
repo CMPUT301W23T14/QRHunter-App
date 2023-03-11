14c86def6f93488d526b344c081c5d656ed617ba
Max Tan <105672769+MaxTan1228@users.noreply.github.com>
2023-03-11 16:58:29 -0700

    Added basic layout for player init

--- QRHunter/app/src/main/java/com/example/qrhunter/ui/auth/PlayerInitFragment.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/ui/auth/PlayerInitFragment.java
@@ -1,5 +1,6 @@
 package com.example.qrhunter.ui.auth;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +11,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.qrhunter.R;
 import com.example.qrhunter.databinding.FragmentPlayerInitBinding;
 import com.example.qrhunter.ui.profile.ProfileViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PlayerInitFragment extends Fragment {
 
@@ -25,6 +28,10 @@ public class PlayerInitFragment extends Fragment {
 
         // Inflate the layout for this fragment
         binding = FragmentPlayerInitBinding.inflate(inflater, container, false);
+
+        // Hide navigation bar
+        BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.bottom_nav);
+        bottomNavigationView.setVisibility(View.GONE);
 
         return binding.getRoot();
     }
