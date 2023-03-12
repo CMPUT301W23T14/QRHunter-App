e594894bed3876941c477d6d42dcb08961198829
Harry Zhao <wpzhao@ualberta.ca>
2023-03-12 16:11:31 -0600

    -new test structures

---
+++ QRHunter/app/src/test/java/com/example/qrhunter/ScanViewModelTest.java
@@ -0,0 +1,27 @@
+package com.example.qrhunter;
+
+import org.junit.jupiter.api.Test;
+
+public class ScanViewModelTest {
+
+    @Test
+    public void checkScanQRCode() {}
+
+    @Test
+    public void checkBitMapToString() {}
+
+    @Test
+    public void checkClearQRCode() {}
+
+    @Test
+    public void checkSetGeolocation() {}
+
+    @Test
+    public void checkAddPhotoLocation() {}
+
+    @Test
+    public void checkClearPhotoLocation() {}
+
+    @Test
+    public void checkGetLocation() {}
+}
