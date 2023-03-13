8f476ec676036121ae2870076507f8722cc18c7e
Harry Zhao <wpzhao@ualberta.ca>
2023-03-13 12:06:56 -0600

    -new test

---
+++ QRHunter/app/src/androidTest/java/com/example/qrhunter/AfterScanFragmentTest.java
@@ -0,0 +1,53 @@
+package com.example.qrhunter;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import android.app.Activity;
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AfterScanFragmentTest {
+
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    /**
+     * Add a city to the listview and check the city name using assertTrue
+     * Clear all the cities from the listview and check again with assertFalse
+     */
+    @Test
+    public void checkList(){
+        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+    }
+
+}
