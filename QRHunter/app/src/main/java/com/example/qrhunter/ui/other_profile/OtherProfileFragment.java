5ec69c22017b7f325233db89a23504c5c36b23f7
lkaijie <kaijie6@ualberta.ca>
2023-03-31 10:20:08 -0600

    fixed QRcodes not sorting when viewing other profiles

--- QRHunter/app/src/main/java/com/example/qrhunter/ui/other_profile/OtherProfileFragment.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/ui/other_profile/OtherProfileFragment.java
@@ -18,6 +18,7 @@ import com.example.qrhunter.databinding.FragmentOtherProfileBinding;
 import com.example.qrhunter.ui.adapters.QRCodesAdapter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * This fragment is for viewing other players' profile page
@@ -65,6 +66,7 @@ public class OtherProfileFragment extends Fragment {
                 otherProfileViewModel.getScannedQRCodes(player).observe(getViewLifecycleOwner(), qrCodes -> {
                     scannedQRCodes.clear();
                     scannedQRCodes.addAll(qrCodes);
+                    scannedQRCodes.sort(Comparator.comparing(qrCode -> qrCode.getName().toLowerCase())); // if we want uppercase first, remove .toLowerCase()
                     qrCodesAdapter.notifyDataSetChanged();
                     binding.scannedText.setText("(" + qrCodes.size() + ")");
                 });
