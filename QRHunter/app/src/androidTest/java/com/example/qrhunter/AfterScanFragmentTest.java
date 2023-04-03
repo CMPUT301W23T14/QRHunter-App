a88d15b8c93db7b58e0745f987ec3d191bee6be6
Harry Zhao <wpzhao@ualberta.ca>
2023-04-03 13:08:02 -0600

    created new test (it works)

--- QRHunter/app/src/androidTest/java/com/example/qrhunter/AfterScanFragmentTest.java
+++ QRHunter/app/src/androidTest/java/com/example/qrhunter/AfterScanFragmentTest.java
@@ -1,5 +1,11 @@
 package com.example.qrhunter;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,22 +15,21 @@ import com.example.qrhunter.data.model.QRCode;
 import com.example.qrhunter.data.repository.PlayerRepository;
 import com.example.qrhunter.data.repository.RepositoryCallback;
 import com.example.qrhunter.ui.leaderboard.LeaderboardViewModel;
+import com.example.qrhunter.ui.profile.ProfileFragmentDirections;
+import com.example.qrhunter.ui.scan.AfterScanFragment;
 import com.example.qrhunter.ui.scan.ScanViewModel;
 
-
-import android.graphics.Bitmap;
-
-import androidx.lifecycle.Observer;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 
 import com.example.qrhunter.data.model.Location;
 import com.example.qrhunter.data.repository.QRCodeRepository;
 import com.example.qrhunter.utils.QRCodeUtil;
-import com.google.firebase.firestore.CollectionReference;
+import com.example.qrhunter.utils.QRCodeVisual;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.robotium.solo.Solo;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -39,19 +44,13 @@ import org.mockito.stubbing.Answer;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 public class AfterScanFragmentTest {
+    private Solo solo;
     private QRCodeRepository qrCodeRepository;
     private PlayerRepository playerRepository;
     private ScanViewModel scanViewModel;
@@ -60,10 +59,9 @@ public class AfterScanFragmentTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
-
     @Before
     public void setup() {
-        // mock the view model and the repository
+        // mock the view model and the repository\
         scanViewModel = Mockito.mock(ScanViewModel.class);
         qrCodeRepository = Mockito.mock(QRCodeRepository.class);
         playerRepository = Mockito.mock(PlayerRepository.class);
@@ -86,28 +84,98 @@ public class AfterScanFragmentTest {
 
 
         activityScenarioRule.getScenario().onActivity(activity -> {
+            scanViewModel = new ViewModelProvider(activity).get(ScanViewModel.class);
             scanViewModel.setQRCodeRepository(qrCodeRepository);
             scanViewModel.setPlayerRepository(playerRepository);
+            NavController navController = Navigation.findNavController(activity, R.id.nav_host_fragment);
+            navController.navigate(R.id.navigation_scan);
+            scanViewModel.scanQRCode("12345678");
+            scanViewModel.setGeolocation(0.01, 0.02);
+            navController.navigate(com.example.qrhunter.ui.scan.ScanFragmentDirections.actionScanFragmentToAfterScanFragment());
         });
 
+        // add delay
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Player testPlayer1 = new Player("fsdf", "John Doe", "1234567890",100);
         db.collection("players").document("TestPlayer").set(testPlayer1);
 
-
         /*
-        ArrayList<Location> locations = new ArrayList<>();
-        Location location = new Location(37.7749, -122.4194);
-        locations.add(location);
-        ArrayList<String> playerId = new ArrayList<>();
-        playerId.add(testPlayer1.getId());
-        QRCode testQRCode1 = new QRCode("1234", "abcdefg", locations, new ArrayList<>(), new ArrayList<>(), playerId);
+
         db.collection("qrCodes").document("TestQRCode").set(testQRCode1);
          */
 
     }
 
+    @Test
+    public void checkForCorrectName() {
+        //solo.clickOnView(solo.getView(R.id.scanButton));
+        String testHash = QRCodeUtil.generateHash("12345678");
+        String testName = QRCodeUtil.generateName(testHash);
+        onView(withId(R.id.QR_name)).check(matches(withText(testName)));
+    }
+
+    /**
+     * This test checks that the correct name is displayed on the QR code fragment.
+     * It generates a hash string and name string for test data, launches the QR code fragment,
+     * and checks that the name displayed on the fragment matches the generated name.
+     */
+    @Test
+    public void checkForCorrectVisual() {
+        //solo.clickOnView(solo.getView(R.id.scanButton));
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location = new Location(37.7749, -122.4194);
+        locations.add(location);
+        ArrayList<String> playerId = new ArrayList<>();
+        Player testPlayer1 = new Player("fsdf", "John Doe", "1234567890",100);
+        playerId.add(testPlayer1.getId());
+        QRCode testQRCode1 = new QRCode("1234", "abcdefg", locations, new ArrayList<>(), new ArrayList<>(), playerId);
+        onView(withId(R.id.QR_name));
+    }
+
+    /**
+     * Test method to check if the correct score is displayed for a QR code content.
+     * Generates a test hash from a sample QR code content, creates a QRCode object from it,
+     * and checks if the score displayed for the QR code matches the expected score generated
+     */
+    @Test
+    public void checkForCorrectScore() {
+        String qrCodeContent = "12345678";
+        String expectedHash = QRCodeUtil.generateHash(qrCodeContent);
+        QRCode qrCode = new QRCode("1", expectedHash, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        List<QRCode> qrCodesList = new ArrayList<>();
+        qrCodesList.add(qrCode);
+        String expectedScore = QRCodeUtil.generateScore(expectedHash) + " Points";
+        onView(withId(R.id.QRCodeScoretext)).check(matches(withText(expectedScore)));
+    }
+
+    /**
+     * This test checks that the user can add multiple geo-locations to a QR code.
+     * It generates test data for a single QR code with no geo-locations, launches the QR code fragment,
+     * and clicks the "Add Geo-Location" button twice to add two geo-locations to the QR code.
+     */
+    @Test
+    public void checkForCorrectLocation() {
+        String qrCodeContent = "Hello, World!";
+        String expectedHash = QRCodeUtil.generateHash(qrCodeContent);
+        QRCode qrCode = new QRCode("1", expectedHash, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        List<QRCode> qrCodesList = new ArrayList<>();
+        qrCodesList.add(qrCode);
+        onView(withId(R.id.addGeoLocationButton)).perform(click());
+        onView(withId(R.id.addGeoLocationButton)).perform(click());
+    }
+
+    /**
+     * This test checks the complete scanning functionality by creating a new QRCode object,
+     * mocking the addQRCodeToPlayer method of the QRCodeRepository,
+     * and calling the completeScan method of the ScanViewModel.
+     * It verifies that the save_button is clicked and the onComplete method of the repositoryCallback
+     */
     @Test
     public void testCompleteScan() {
         String qrCodeId = "123";
@@ -134,114 +202,8 @@ public class AfterScanFragmentTest {
         }).when(qrCodeRepository).addQRCodeToPlayer(eq(qrCode), eq(playerId), eq(null), eq(repositoryCallback));
 
         // Call the completeScan method
+        onView(withId(R.id.addGeoLocationButton)).perform(click());
         scanViewModel.completeScan(qrCodeId, playerId, null, repositoryCallback);
+        onView(withId(R.id.save_button)).perform(click());
     }
-
-    @Test
-    public void scanQRCode_updatesQRCodeContentAndHash() {
-        String qrCodeContent = "Hello, World!";
-        String expectedHash = QRCodeUtil.generateHash(qrCodeContent);
-        QRCode qrCode = new QRCode("1", expectedHash, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-        List<QRCode> qrCodesList = new ArrayList<>();
-        qrCodesList.add(qrCode);
-        MutableLiveData<List<QRCode>> qrCodesLiveData = new MutableLiveData<>();
-        scanViewModel.scanQRCode(qrCodeContent);
-        String result = QRCodeUtil.generateHash(qrCodeContent);
-        verify(scanViewModel.getQrCodeLiveDataHash()).setValue(result);
-    }
-
-    /*
-    @Test
-    public void completeScan_addsNewQRCodeToPlayer() {
-        String playerId = "player1";
-        String qrCodeHash = "hash123";
-        Location location = new Location(0, 0, new ArrayList<>());
-        ArrayList<String> playerIds = new ArrayList<String>();
-        playerIds.add(playerId);
-        QRCode expectedQRCode = new QRCode("", qrCodeHash, location, new ArrayList<>(), playerIds);
-
-        scanViewModel.scanQRCode(qrCodeHash);
-        scanViewModel.completeScan(playerId);
-
-        verify(qrCodeRepository).addQRCodeToPlayer(expectedQRCode, playerId);
-    }
-
-    @Test
-    public void clearQRCode_resetsQRCodeContentAndHash() {
-        scanViewModel.clearQRCode();
-        verify(qrCodeContentObserver).onChanged("");
-        verify(qrCodeHashObserver).onChanged("");
-    }
-
-    @Test
-    public void setGeolocation_updatesLocation() {
-        double latitude = 37.7749;
-        double longitude = -122.4194;
-        Location expectedLocation = new Location(latitude, longitude, new ArrayList<>());
-
-        scanViewModel.setGeolocation(latitude, longitude);
-
-        verify(locationObserver).onChanged(expectedLocation);
-    }
-
-    @Test
-    public void addPhotoLocation_updatesLocation() {
-        Bitmap photo = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        String photoString = scanViewModel.BitMapToString(photo);
-        Location expectedLocation = new Location(0, 0, new ArrayList<>());
-        expectedLocation.photos.add(photoString);
-
-        scanViewModel.addPhotoLocation(photo);
-
-        verify(locationObserver).onChanged(expectedLocation);
-    }
-
-    @Test
-    public void addPhotoLocation_addsPhotoToLocationPhotos() {
-        ScanViewModel viewModel = new ScanViewModel();
-        Bitmap photo = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        viewModel.addPhotoLocation(photo);
-        List<String> photos = viewModel.getLocation().getValue().getPhotos();
-        assertNotNull(photos);
-        assertEquals(1, photos.size());
-        assertEquals(viewModel.BitMapToString(photo), photos.get(0));
-    }
-
-    @Test
-    public void addPhotoLocation_addsMultiplePhotosToLocationPhotos() {
-        ScanViewModel viewModel = new ScanViewModel();
-        Bitmap photo1 = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Bitmap photo2 = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
-        viewModel.addPhotoLocation(photo1);
-        viewModel.addPhotoLocation(photo2);
-        ArrayList<String> photos = viewModel.getLocation().getValue().getPhotos();
-        assertNotNull(photos);
-        assertEquals(2, photos.size());
-        assertEquals(viewModel.BitMapToString(photo1), photos.get(0));
-        assertEquals(viewModel.BitMapToString(photo2), photos.get(1));
-    }
-
-    @Test
-    public void clearPhotoLocation_removesAllPhotosFromLocationPhotos() {
-        ScanViewModel viewModel = new ScanViewModel();
-        Bitmap photo1 = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Bitmap photo2 = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
-        viewModel.addPhotoLocation(photo1);
-        viewModel.addPhotoLocation(photo2);
-        viewModel.clearPhotoLocation();
-        ArrayList<String> photos = viewModel.getLocation().getValue().getPhotos();
-        assertNotNull(photos);
-        Assert.assertTrue(photos.isEmpty());
-    }
-
-    @Test
-    public void getLocation_returnsMutableLiveDataOfLocation() {
-        ScanViewModel viewModel = new ScanViewModel();
-        assertNotNull(viewModel.getLocation());
-        Assert.assertTrue(viewModel.getLocation() instanceof MutableLiveData<?>);
-        Assert.assertTrue(viewModel.getLocation().getValue() instanceof Location);
-    }
-
-     */
-
 }
