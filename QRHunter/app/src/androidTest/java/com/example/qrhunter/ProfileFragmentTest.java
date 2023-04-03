de8c1b32a9b97c01864ccd7a5b81e5bc587d966d
Siddharth-Chhatbar <chhatbar@ualberta.ca>
2023-04-03 10:59:41 -0600

    This should work as a test for inputting phone number

--- QRHunter/app/src/androidTest/java/com/example/qrhunter/ProfileFragmentTest.java
+++ QRHunter/app/src/androidTest/java/com/example/qrhunter/ProfileFragmentTest.java
@@ -1,5 +1,4 @@
 package com.example.qrhunter;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -7,6 +6,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withInputType;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.junit.Assert.assertTrue;
@@ -23,21 +23,17 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-public class QRCodeFragmentTest {
+public class ProfileFragmentTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
     private Solo solo;
 
     @Before
     public void setup() {
-        // Launch the qr code fragment
+        // Launch the profile fragment
         activityScenarioRule.getScenario().onActivity(activity -> {
             NavController navController = Navigation.findNavController(activity, R.id.nav_host_fragment);
             navController.navigate(R.id.navigation_profile);
-
-            com.example.qrhunter.ui.profile.ProfileFragmentDirections.ActionNavigationProfileToQrCodeFragment action =
-                    ProfileFragmentDirections.actionNavigationProfileToQrCodeFragment("123");
-            navController.navigate(action);
         });
         // add delay
         try {
@@ -46,21 +42,12 @@ public class QRCodeFragmentTest {
             e.printStackTrace();
         }
     }
-
     @Test
-    public void fragment_can_be_instantiated() {
-        // Wait for the leaderboard fragment to be displayed
-        onView(withId(R.id.QR_name)).check(matches(withText("Name")));
-        onView(withId(R.id.QRCodeScoretext)).check(matches(withText("0.0")));
-    }
-
-    @Test
-    public void adding_comment() {
-        // Type and enter comment
-        onView(withId(R.id.new_comment_edit_text)).perform(typeText("New comment"));
-        onView(withContentDescription("add comment")).perform(click());
-
-        // Check whether comment exist. FloMoMegaSpectralCrab is the default author name
-        onView(withText("New comment")).check(matches(isDisplayed()));
+    public void add_phone_number(){
+        // Type and enter phone number
+        onView(withId(R.id.phone_number_edit_text)).perform(typeText("000222333"));
+        Espresso.closeSoftKeyboard();
+        // Check whether the phone number is the one correctly inputted
+        onView(withId(R.id.phone_number_edit_text)).check(matches(withText("000222333")));
     }
 }
